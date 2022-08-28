6177b7f99f92f3eff2d96ffadcbab9d860af47f5
dhanykoinworks <dhany.aritonang@koinworks.com>
2022-08-28 15:20:10 +0700

    test ping connection

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func handlerTest(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Add("content-type", "application/json")
+	response := map[string]string{
+		"message": "success",
+	}
+
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(response)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("hallo")
+	router := httprouter.New()
+
+	router.GET("/ping", handlerTest)
+
+	err := http.ListenAndServe(":11010", router)
+	if err != nil {
+		panic(err)
+	}
 }
